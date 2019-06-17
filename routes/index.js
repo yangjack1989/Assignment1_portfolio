a697e564b34ade89a8e6acbbcea01a053d9dade7
yangjack1989 <200362300@student.georgianc.on.ca>
2019-06-17 16:39:31 -0400

    create pug files for each page and narbar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'my about page' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'my project page' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'my serivces page' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'my contact page' });
+});
+
 module.exports = router;
